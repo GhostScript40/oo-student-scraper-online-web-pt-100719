e0ffea4893ddfbb053c2397289e84659d01f85e7
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-25 22:33:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     roster = []
     html = Nokogiri::HTML(open(index_url))
-    binding.pry
+  
   end
  
   def self.scrape_profile_page(profile_url)
